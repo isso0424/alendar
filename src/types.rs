18ca3a2a3415456567e1d9854d9af116f5a13a5d
isso0424 <isso04240129@gmail.com>
2023-06-13 11:25:33 +0900

    feat: define Note

--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,37 @@
+enum SimpleNote {
+    Top,
+    Middle,
+    Base,
+}
+
+pub enum Note {
+    SimpleNote(SimpleNote),
+    TopAndMiddle,
+    MiddleAndBase,
+}
+
+impl Note {
+    fn satisfy(&self, note: SimpleNote) -> bool {
+        match self {
+            SimpleNote(n) => n == note,
+            Self::TopAndMiddle => note == SimpleNote::Top || note == SimpleNote::Middle,
+            Self::MiddleAndBase => note == SimpleNote::Middle || note == SimpleNote::Base,
+        }
+    }
+}
+
 pub struct EssentialOil {
     name: String,
-    // TODO: add note
+    note: Note,
     // TODO: add type
     // TODO: add effect
 }
 
 impl EssentialOil {
-    pub fn new(name: &str) -> Self {
+    pub fn new(name: &str, note: Note) -> Self {
         Self {
             name: name.to_string(),
+            note,
         }
     }
 }

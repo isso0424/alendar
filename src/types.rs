8fafe6f58dd11d4e09adb80e3cec655ef916318e
isso0424 <isso04240129@gmail.com>
2023-06-13 12:29:02 +0900

    feat: define type of Family

--- src/types.rs
+++ src/types.rs
@@ -1,37 +1,71 @@
-enum SimpleNote {
+use bitflags::bitflags;
+
+#[allow(unused)]
+#[derive(PartialEq, Eq)]
+pub enum SimpleNote {
     Top,
     Middle,
     Base,
 }
 
+#[allow(unused)]
+#[derive(PartialEq, Eq)]
 pub enum Note {
     SimpleNote(SimpleNote),
     TopAndMiddle,
     MiddleAndBase,
 }
 
+#[allow(unused)]
 impl Note {
     fn satisfy(&self, note: SimpleNote) -> bool {
         match self {
-            SimpleNote(n) => n == note,
+            Self::SimpleNote(n) => &note == n,
             Self::TopAndMiddle => note == SimpleNote::Top || note == SimpleNote::Middle,
             Self::MiddleAndBase => note == SimpleNote::Middle || note == SimpleNote::Base,
         }
     }
 }
 
+bitflags! {
+    #[derive(Clone)]
+    pub struct Family: u8 {
+        const Woody =   0b00000001;
+        const Froral =  0b00000010;
+        const Resin =   0b00000100;
+        const Herball = 0b00001000;
+        const Spicy =   0b00010000;
+        const Citrus  = 0b00100000;
+        const Fruity  = 0b01000000;
+    }
+}
+
+#[allow(unused)]
+impl Family {
+    fn add(&self, new: Self) -> Self {
+        self.to_owned().union(new)
+    }
+
+    fn satisfy(&self, family: Self) -> bool {
+        self.intersects(family)
+    }
+}
+
+#[allow(unused)]
 pub struct EssentialOil {
     name: String,
     note: Note,
-    // TODO: add type
+    family: Family,
     // TODO: add effect
 }
 
+#[allow(unused)]
 impl EssentialOil {
-    pub fn new(name: &str, note: Note) -> Self {
+    pub fn new(name: &str, note: Note, family: Family) -> Self {
         Self {
             name: name.to_string(),
             note,
+            family,
         }
     }
 }

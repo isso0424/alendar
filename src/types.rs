e464dfd00c528880e5276cf784324e1515f910bc
isso0424 <isso04240129@gmail.com>
2023-06-15 14:39:00 +0900

    feat: add calculator for family

--- src/types.rs
+++ src/types.rs
@@ -28,15 +28,39 @@ impl Note {
 }
 
 bitflags! {
-    #[derive(Clone)]
+    #[derive(Clone, Copy, Default)]
     pub struct Family: u8 {
-        const Woody =   0b00000001;
-        const Froral =  0b00000010;
-        const Resin =   0b00000100;
-        const Herball = 0b00001000;
-        const Spicy =   0b00010000;
-        const Citrus  = 0b00100000;
-        const Fruity  = 0b01000000;
+        const One     = 0b0000001;
+        const Citrus  = 0b0000001;
+        const Froral =  0b0000010;
+        const Herball = 0b0000100;
+        const Wood    = 0b0001000;
+        const Resin =   0b0010000;
+        const Spicy =   0b0100000;
+        const Earthy =  0b1000000;
+        const Max     = 0b1000000;
+    }
+}
+
+impl From<u8> for Family {
+    fn from(item: u8) -> Self {
+        Family::from_bits_retain(item)
+    }
+}
+
+impl std::ops::Shr for Family {
+    type Output = Self;
+
+    fn shr(self, rhs: Self) -> Self {
+        Family::from(self.bits() >> rhs.bits())
+    }
+}
+
+impl std::ops::Shl for Family {
+    type Output = Self;
+
+    fn shl(self, rhs: Self) -> Self {
+        Family::from(self.bits() << rhs.bits())
     }
 }
 
@@ -48,6 +72,39 @@ impl Family {
 
     fn satisfy(&self, family: Self) -> bool {
         self.intersects(family)
+    }
+
+    fn r_shift(&mut self) {
+        let mut tmp = self.clone() >> Self::One;
+        if (self.intersects(Self::One)) {
+            tmp = tmp.add(Self::Max);
+        }
+
+        *self = tmp;
+    }
+
+    fn l_shift(&mut self) {
+        let mut tmp = self.clone() << Self::One;
+        if (self.intersects(Self::Max)) {
+            tmp = tmp.add(Self::One);
+        }
+
+        *self = tmp;
+    }
+
+    fn distance(&self, family: Self) -> usize {
+        let mut i = 0;
+        let mut l = self.clone();
+        let mut r = self.clone();
+
+        loop {
+            if (l.intersects(family) || r.intersects(family)) {
+                return i;
+            }
+            l.l_shift();
+            r.r_shift();
+            i += 1;
+        }
     }
 }
 
